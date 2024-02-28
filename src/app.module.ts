ed02d25f51d22fd800473cda027dc32d46d617ef
Mohamed Singano <singano2009@gmail.com>
2024-02-28 04:20:58 -0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogsController } from './controllers/logs/logs.controller';
+import { LogsService } from './services/logs/logs.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LogsController],
+  providers: [AppService, LogsService],
 })
 export class AppModule {}
