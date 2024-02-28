b25e71d3916aee9c5c1e396eed1ea1b5fe3b9390
Mohamed Singano <singano2009@gmail.com>
2024-02-28 04:23:46 -0800

    added codebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsController } from './controllers/logs/logs.controller';
 import { LogsService } from './services/logs/logs.service';
+import { TestController } from './controllers/test/test.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, LogsController],
+  controllers: [AppController, LogsController, TestController],
   providers: [AppService, LogsService],
 })
 export class AppModule {}
