fd8b23617a670f9707451c9d0ce91e69f532cff6
Fredy <fredgerman30@gmail.com>
2024-03-04 02:07:01 +0300

    chore : .git ignore update

--- dist/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from './app.service';
-export declare class IName {
-    name: string;
-}
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(query: IName): string;
-}
