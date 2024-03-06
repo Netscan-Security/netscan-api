3cd65bf50870d4cd663a04523d48bbc10a14cec0
Fredy Mgimba <fredgerman30@gmail.com>
2024-03-06 20:45:53 +0300

    feat : added users and organisations modals

--- src/modules/drizzle/schema.ts
+++ src/modules/drizzle/schema.ts
@@ -1,21 +1,11 @@
-import { relations } from 'drizzle-orm';
-import { serial, pgTable, varchar, integer } from 'drizzle-orm/pg-core';
-// import { relations } from 'drizzle-orm';
+import { organizationUser } from './tables/organizationUser';
+import { organizations, organizationRelations } from './tables/organizations';
+import { users, usersRelations } from './tables/users';
 
-export const countries = pgTable('countries', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 256 }),
-});
-export const cities = pgTable('cities', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 256 }),
-  countryId: integer('country_id').references(() => countries.id),
-});
-
-export const cityRelation = relations(cities, ({ one }) => ({
-  country_city: one(countries, {
-    fields: [cities.countryId],
-    references: [countries.id],
-  }),
-}));
-
+export {
+  users,
+  usersRelations,
+  organizations,
+  organizationRelations,
+  organizationUser,
+};
---
+++ src/modules/drizzle/tables/organizationUser.ts
@@ -0,0 +1,18 @@
+import { pgTable, primaryKey, uuid } from 'drizzle-orm/pg-core';
+import { organizations } from './organizations';
+import { users } from './users';
+
+export const organizationUser = pgTable(
+  'organization_user',
+  {
+    userId: uuid('user_id')
+      .notNull()
+      .references(() => users.id),
+    organizationId: uuid('organization_id')
+      .notNull()
+      .references(() => organizations.id),
+  },
+  (t) => ({
+    pk: primaryKey(t.userId, t.organizationId),
+  }),
+);
