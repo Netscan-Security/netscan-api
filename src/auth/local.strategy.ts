9037c5bb757f0b2d4cc12d082db8146985acbf28
Fredy <fredgerman30@gmail.com>
2024-03-11 09:09:02 +0300

    wip

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -10,6 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
+    Logger.log('LocalStrategy.validate()');
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
---
+++ src/interfaces/dtos/users.interface.dto.ts
@@ -0,0 +1,7 @@
+export class GetUsersDto {
+  /**
+   * The id of the user
+   * @example 'user1'
+   */
+  id: string;
+}
