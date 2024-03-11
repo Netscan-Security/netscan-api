4aed193902983d9fe6d0acc938149292b23d7aa7
Fredy <fredgerman30@gmail.com>
2024-03-11 07:28:45 +0300

    feat : JWT secret from env, and env config update

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,4 +1,5 @@
+import config from 'src/config';
+
 export const jwtConstants = {
-  secret:
-    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+  secret: config.jwtSecret,
 };
