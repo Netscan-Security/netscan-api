23773b9eab419faec58d1e594dd9ebe60d86a991
Fredy <fredgerman30@gmail.com>
2024-03-17 09:33:34 +0300

    feat : updated swagger docs info on building

--- src/interfaces/dtos/building.interface.dto.ts
+++ src/interfaces/dtos/building.interface.dto.ts
@@ -3,12 +3,24 @@ import { IsNotEmpty, IsString, IsObject, IsUUID } from 'class-validator';
 export class CreateBuildingDto {
   @IsNotEmpty()
   @IsString()
+  /**
+   * The name of the building
+   * @example 'Building 1'
+   */
   name: string;
 
   @IsNotEmpty()
   @IsObject()
+  /**
+   * The metadata of the building
+   * @example { "floors": 5, "rooms": 20 }
+   */
   metadata: object;
 
   @IsUUID()
+  /**
+   * The campus id of the building, must be uuid of a valid campus
+   * @example 'fd5c008f-a521-49a4-9727-ac44c0deaa58'
+   */
   campusId: string;
 }
